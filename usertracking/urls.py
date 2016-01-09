f94f013c17582952ad4da9b71588f3402cfda206
joebos <frank_lqx@yahoo.com>
2016-01-09 01:35:48 +0000

    add csrf exempt

--- usertracking/urls.py
+++ usertracking/urls.py
@@ -1,9 +1,10 @@
 
 from django.conf.urls import url, patterns
 from views import VerifyView, RegisterEventView
+from django.views.decorators.csrf import csrf_exempt
 
 urlpatterns = patterns('',
     url(r'^user-tracking/verify$', VerifyView.as_view(), name='user_tracking_verify'),
-    url(r'^user-tracking/register-event$', RegisterEventView.as_view(), name='user_tracking_register_event'),
+    url(r'^user-tracking/register-event$', csrf_exempt(RegisterEventView.as_view()), name='user_tracking_register_event'),
 )
 
